3c588b5fe78decf5069176b79a6949d7fd7606b4
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-08-11 11:56:47 +0900

    Implemented Register method of SensorManager.

--- sensor/app.py
+++ sensor/app.py
@@ -6,20 +6,50 @@ import grpc
 
 from services import sensor_manager_pb2, sensor_manager_pb2_grpc
 
+import mysql.connector as db
+
 N_DUMMY_SENSORS = 2
 DUMMY_ROOM_ID = 1
 DUMMY_HOST = '127.0.0.1:8080'
 DUMMY_SUCCESS = True
+
+logger = logging.getLogger(__name__)
 
 class SensorManagerServicer(sensor_manager_pb2_grpc.SensorManagerServicer):
     def __init__(self, *args, **kwargs):
         super(SensorManagerServicer, self).__init__(*args, **kwargs)
 
     def Register(self, request, context):
-        sensors = [sensor_manager_pb2.Sensor(room_id=DUMMY_ROOM_ID,
-                                             host=DUMMY_HOST)]
+        try:
+            cnx = db.connect(
+                host='sensor-db',
+                port='3306',
+                user='root',
+                password='password',
+                database='sensors'
+            )
+            cur = cnx.cursor()
+            cur.execute('insert into sensors values (null, %s, %s)',
+                        (request.room_id, request.host))
+            id = cur.lastrowid
+            cnx.commit()
+        except db.Error as e:
+            logger.debug('DB error.', exc_info=True)
+            success = False
+        else:
+            cur.close()
+            cnx.close()
+            success = True
+
+        if success:
+            sensors = [sensor_manager_pb2.Sensor(room_id=id,
+                                                 host=request.host)]
+        else:
+            sensors = [sensor_manager_pb2.Sensor(room_id=None,
+                                                 host=request.host)]
         return sensor_manager_pb2.Result(sensors=sensors,
-                                         success=DUMMY_SUCCESS)
+                                         success=success)
+
 
     def Get(self, request, context):
         sensors = [sensor_manager_pb2.Sensor(room_id=DUMMY_ROOM_ID,
@@ -37,5 +67,5 @@ def serve():
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(level=logging.DEBUG)
     serve()
