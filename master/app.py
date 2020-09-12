cf564c5922625b1ea4c439a6438c6350961c9cba
AltTether <245134e@gs.kochi-tech.ac.jp>
2020-09-13 00:22:04 +0900

    センサー情報の取得部分を実際の動作に修正

--- master/app.py
+++ master/app.py
@@ -12,34 +12,48 @@ DUMMY_PASSWORD='password'
 DUMMY_RASP1_ID=1
 DUMMY_RASP2_ID=2
 DUMMY_HOST='127.0.0.1:8080'
+DEFAULT_PORT = 50051
+TIMEOUT = 5
 
 app = Flask(__name__)
 
-@app.route('/room', methods=['GET'])
-def get_states():
+@app.route('/room/<room_id>', methods=['GET'])
+def get_states(room_id):
+    password = request.args.get('password')
+    if password is None:
+        return Response(response='Bad Request', status=400)
+
     with grpc.insecure_channel('room:50051') as channel:
         stub = room_manager_pb2_grpc.RoomManagerStub(channel)
-        room = room_manager_pb2.Room(id=DUMMY_ROOM_ID,
-                                     password=DUMMY_PASSWORD)
-        _ = stub.Signin(room)
+        room = room_manager_pb2.Room(id=int(room_id),
+                                     password=password)
+        result = stub.Signin(room)
+
+    if not result.success:
+        return Response(response='Bad Request', status=400)
 
     with grpc.insecure_channel('sensor:50051') as channel:
         stub = sensor_manager_pb2_grpc.SensorManagerStub(channel)
         sensor = sensor_manager_pb2.Sensor(room_id=DUMMY_ROOM_ID)
-        _ = stub.Get(sensor)
+        result = stub.Get(sensor)
+
+    if not result.success:
+        return Response(response='Internal Server Error', status=500)
 
     resp = {}
-    with grpc.insecure_channel('rasp1:50051') as channel:
-        stub = rasp_pb2_grpc.RaspStub(channel)
-        empty = rasp_pb2.Empty()
-        state = stub.GetState(empty)
-        resp[DUMMY_RASP1_ID] = {'state': {'opened': state.opened}}
+    # FIX センサーのデータにIDが存在しないため,一旦インデックスで代用
+    # messageが修正されたらIDに修正
 
-    with grpc.insecure_channel('rasp2:50051') as channel:
-        stub = rasp_pb2_grpc.RaspStub(channel)
-        empty = rasp_pb2.Empty()
-        state = stub.GetState(empty)
-        resp[DUMMY_RASP2_ID] = {'state': {'opened': state.opened}}
+    for idx, sensor in enumerate(result.sensors):
+        addr = '{}:{}'.format(sensor.host, DEFAULT_PORT)
+        with grpc.insecure_channel(addr) as channel:
+            stub = rasp_pb2_grpc.RaspStub(channel)
+            empty = rasp_pb2.Empty()
+            try:
+                state = stub.GetState(empty, timeout=TIMEOUT)
+                resp[idx] = {'state': {'opened': state.opened}}
+            except:
+                resp[idx] = {'state': {'opened': None}}
 
     return Response(response=json.dumps(resp), status=200)
 
