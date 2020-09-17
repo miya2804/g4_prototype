22455ea739f4d2a6264827b3e518070ec04b9aa6
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-09-17 16:54:41 +0900

    mysqlのconnection取得をメソッドにまとめる

--- sensor/dbmanager.py
+++ sensor/dbmanager.py
@@ -14,13 +14,7 @@ class SensordbManager():
 
     def register_sensor(self, room_id, host):
         try:
-            cnx = mysql.connector.connect(
-                host=self.config.get(self.section, 'host'),
-                port=self.config.getint(self.section, 'port'),
-                user=self.config.get(self.section, 'user'),
-                password=self.config.get(self.section, 'password'),
-                database=self.config.get(self.section, 'database')
-            )
+            cnx = self._connect()
             cur = cnx.cursor()
             cur.execute('insert into sensors values (null, %s, %s)',
                         (room_id, host))
@@ -39,13 +33,7 @@ class SensordbManager():
 
     def get_sensors(self, room_id):
         try:
-            cnx = mysql.connector.connect(
-                host=self.config.get(self.section, 'host'),
-                port=self.config.getint(self.section, 'port'),
-                user=self.config.get(self.section, 'user'),
-                password=self.config.get(self.section, 'password'),
-                database=self.config.get(self.section, 'database')
-            )
+            cnx = self._connect()
             cur = cnx.cursor()
             cur.execute('select room_id,host from sensors where room_id= %s',
                         (room_id, ))
@@ -60,3 +48,13 @@ class SensordbManager():
             success = True
 
         return sensors, success
+
+    def _connect(self):
+        cnx = mysql.connector.connect(
+            host=self.config.get(self.section, 'host'),
+            port=self.config.getint(self.section, 'port'),
+            user=self.config.get(self.section, 'user'),
+            password=self.config.get(self.section, 'password'),
+            database=self.config.get(self.section, 'database')
+        )
+        return cnx
