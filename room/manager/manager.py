84225e4b9a1be868d12ce987f30eead8411eacd9
AltTether <245134e@gs.kochi-tech.ac.jp>
2020-09-23 17:56:13 +0900

    ファイルからコンフィグを読み込んでインスタンスを作成する

--- room/manager/manager.py
+++ room/manager/manager.py
@@ -1,6 +1,11 @@
+import configparser
+
 from orator import DatabaseManager
 from models import Room, set_connection
 
+CONFIG_NAME = 'mysql'
+MYSQL_DRIVER_NAME = 'mysql'
+CONFIG_SECTION = 'room-db'
 
 class Manager():
     def __init__(self, db_config):
@@ -25,3 +30,21 @@ class Manager():
         if len(room) == 1:
             return True
         return False
+
+    @classmethod
+    def from_file(cls, path):
+        config = configparser.ConfigParser()
+        config.read(path)
+        
+        db_config = {
+            CONFIG_NAME: {
+                'driver': MYSQL_DRIVER_NAME,
+                'host': config.get(CONFIG_SECTION, 'host'),
+                'database': config.get(CONFIG_SECTION, 'database'),
+                'user': config.get(CONFIG_SECTION, 'user'),
+                'password': config.get(CONFIG_SECTION, 'password'),
+                'prefix': '',
+            }
+        }
+        
+        return cls(db_config)
