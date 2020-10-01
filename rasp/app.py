317594fa2c0a58e95bdb7817b537057540615e04
AltTether <200351p@ugs.kochi-tech.ac.jp>
2020-10-01 17:55:05 +0900

    サーバとセンサーを分離して，外側からセンサーのアルゴリズムだけを追加する

--- rasp/app.py
+++ rasp/app.py
@@ -10,27 +10,46 @@ from flask import Flask, Response, request
 
 from services import rasp_pb2, rasp_pb2_grpc
 
-STATE=True
+
+class Rasp():
+    def __init__(self):
+        self.opened = True
+
+    def get_state(self):
+        pass
+
+    def set_state(self, opened):
+        pass
 
 
-class RaspServicer(rasp_pb2_grpc.RaspServicer):
-    def GetState(self, request, context):
-        opened=STATE
-        return rasp_pb2.State(opened=opened)
+class VirtualRasp(Rasp):
+    def get_state(self):
+        return self.opened
 
-def serve_as_grpc():
+    def set_state(self, opened):
+        self.opened = opened
+
+
+def get_rasp_servicer(rasp):
+    class RaspServicer():
+        def GetState(self, request, context):
+            opened = rasp.get_state()
+            return rasp_pb2.State(opened=opened)
+    
+    return RaspServicer()
+
+
+def serve_as_grpc(servicer):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    rasp_pb2_grpc.add_RaspServicer_to_server(RaspServicer(), server)
+    rasp_pb2_grpc.add_RaspServicer_to_server(servicer, server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
 
-def serve():
-    app = Flask(__name__)
-
+def register_route(app, rasp):
     @app.route('/', methods=['GET'])
     def get():
-        opened = STATE
+        opened = rasp.get_state()
 
         resp = {'opened': opened}
         return Response(response=json.dumps(resp),
@@ -43,17 +62,19 @@ def serve():
         if open_ is None:
             return Response(response='Bad Request',
                             status=HTTPStatus.BAD_REQUEST)
-        STATE = open_
-
-        if STATE:
-            opened = True
-        else:
-            opened = False
+        rasp.set_state(open_)
 
         resp = {'success': True,
-                'opened': opened}
+                'opened': open_}
         return Response(response=json.dumps(resp),
                         status=HTTPStatus.OK)
+
+    return app
+
+def serve(rasp):
+    app = Flask(__name__)
+
+    register_route(app, rasp)
 
     app.run('0.0.0.0', port=3000)
 
@@ -67,7 +88,10 @@ if __name__ == '__main__':
     args = parse_args()
     logging.basicConfig()
 
+    rasp = VirtualRasp()
+
     if args.grpc:
-        serve_as_grpc()
+        servicer = get_rasp_servicer(rasp)
+        serve_as_grpc(servicer)
     else:
-        serve()
+        serve(rasp)
