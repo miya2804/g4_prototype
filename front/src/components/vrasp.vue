4043bf67144f6bfddbc07f54657e7277c35d8949
AltTether <245134e@gs.kochi-tech.ac.jp>
2020-10-02 23:11:33 +0900

    [WIP]vue-routerによるSPAへの変更

---
+++ front/src/components/vrasp.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+  hoge!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  <p><router-link to="/">Top</router-link></p>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,26 @@
 import Vue from 'vue/dist/vue.esm.js';
+import VueRouter from 'vue-router'
 
 import App from './components/App.vue';
+import MasterPage from './components/master.vue';
+import VRaspPage from './components/vrasp.vue';
+
+
+Vue.use(VueRouter);
+
+
+const routes = [
+    { path: '/', component:  MasterPage },
+    { path: '/vrasp', component: VRaspPage },
+];
+
+const router = new VueRouter({
+    routes,
+});
 
 new Vue({
-    el: '#app',
+    router,
     components: { App },
-    template: '<app/>'
-})
+    el: '#app',
+    template: '<App/>',
+});
